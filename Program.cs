d8febfbbffaca16c4086a8e4a8a2f3fbf841de28
Hemant-Chauhan <40966812+Hemant-Chauhan@users.noreply.github.com>
2023-01-27 15:54:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmsk0154Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person();
+            Console.WriteLine(bob.IsValidated());
+
+            Person kim = new Person("kim", "smith");
+            Console.WriteLine(kim.IsValidated());
+            Console.ReadLine();
+        }
+    }
+}
